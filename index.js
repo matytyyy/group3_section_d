ce5af8a6fa143bdfa06028ea2e8b548e6acba59c
matytyyy <mattijan18@gmail.com>
2026-09-22 14:14:03 +0800

    git commit -m "feat: to be continued"

--- index.js
+++ index.js
@@ -5,12 +5,11 @@ const app = express()
 
 app.use(express.json())
 
-app.use('/', require('./routes/users.routes'))
-app.use('/api/users', require('./routes/users.routes'))
-app.use('/api/products', require('./routes/users.routes'))
-app.use('/api/orders', require('./routes/users.routes'))
-app.use('/api/categories', require('./routes/users.routes'))
-app.use('/api/notifications', require('./routes/users.routes'))
+app.use('/api/users', require('./users/users.routes'))
+app.use('/api/products', require('./routes/products.routes'))
+app.use('/api/orders', require('./routes/orders.routes'))
+app.use('/api/categories', require('./routes/categories.routes'))
+app.use('/api/notifications', require('./routes/notifications.routes'))
 
 app.listen(4321, () => {
 	console.log('Server is running on http://localhost:4321')
